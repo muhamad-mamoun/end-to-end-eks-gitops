093a414de9e07a6d83e4de95f9422725611d4e31
Muhamad Mamoun <muhamadmamoun@gmail.com>
2025-06-09 02:40:56 +0300

    feat(terraform): add kubectl provider to terraform

--- Terraform/Modules/Kubernetes/providers.tf
+++ Terraform/Modules/Kubernetes/providers.tf
@@ -8,3 +8,14 @@ provider "kubernetes" {
     args        = ["eks", "get-token", "--profile", var.aws-profile, "--cluster-name", var.cluster-name]
   }
 }
+
+provider "kubectl" {
+  cluster_ca_certificate = base64decode(var.cluster-certificate)
+  host                   = var.cluster-endpoint
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--profile", var.aws-profile, "--cluster-name", var.cluster-name]
+  }
+}
